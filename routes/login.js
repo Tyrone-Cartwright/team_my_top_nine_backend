604ecdf1ee65b111b52a5a7e748d49ce0981de93
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-02-04 04:23:15 -0500

    got GET/POST /api/users and /api/register to work

--- routes/login.js
+++ routes/login.js
@@ -12,4 +12,31 @@ function protected(req, res, next) {
   }
 }
 
+router.get("/users", (req, res) => {
+  db("users")
+    .select("id", "username")
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+router.post("/register", (req, res) => {
+  //Grab the username and password from the body
+  const creds = req.body;
+  // generate the hash from the users password
+  const hash = bcrypt.hashSync(creds.password, 15);
+  // override the user password with the hash
+  creds.password = hash;
+  // save the user to the db
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      req.session.username = creds.username;
+      res.status(201).json({ newUserId: id });
+    })
+    .catch(err => res.json(err));
+});
+
 module.exports = router;
